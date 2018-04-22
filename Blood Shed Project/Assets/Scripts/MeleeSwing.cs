76e4924026193478a216a981a0349f73d227a90a
EvoIsGod <35813940+EvoIsGod@users.noreply.github.com>
2018-04-22 02:18:46 +0100

    wep
    Made wrench swingable

---
+++ Blood Shed Project/Assets/Scripts/MeleeSwing.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeSwing : MonoBehaviour {
+
+	public Animation meleeAnims;
+
+	public float animNum;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			animNum = Random.Range (0, 4);
+		} else {
+			animNum = 0;
+		}
+
+
+		if (animNum == 1) {
+			meleeAnims.Play ("Swing1");
+		}
+		if (animNum == 2) {
+			meleeAnims.Play ("Swing2");
+		}
+		if (animNum == 3) {
+			meleeAnims.Play ("Swing3");
+		}
+	}
+}
